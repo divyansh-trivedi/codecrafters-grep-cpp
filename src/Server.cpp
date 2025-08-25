ac68afce9b8c839f6e793fc55939050e511c5422
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-08-25 16:05:33 +0530

    4thTask

--- src/Server.cpp
+++ src/Server.cpp
@@ -12,7 +12,7 @@ bool match_pattern(const string& input_line, const string& pattern) {
         return input_line.find_first_of("0123456789") != string::npos;
     }
     else if(pattern == "\\w"){
-        return input_line.find_first_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_") != std::string::npos;
+        return input_line.find_first_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_") != string::npos;
     }
     else if(pattern.size() >=3 && pattern[0] == '[' && pattern[pattern.size()-1] == ']'){
         string str = pattern.substr(1,pattern.size()-2);
