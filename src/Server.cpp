6bf167b98b0cd0e23351f445f638c57d73782dfe
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-08-27 10:14:43 +0530

    12thTask

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,140 +1,110 @@
 #include <iostream>
 #include <string>
+#include <stdexcept>
+#include <cctype>
 
 using namespace std;
-bool  isDigit(char c){
-    return c>='0' && c<='9';
+
+bool isDigit(char c){
+    return isdigit(c);
 }
-bool  isAlpha(char c){
-    return c>='a' && c<='z';
+
+bool isAlpha(char c){
+    return isalpha(c);
 }
+
 bool match_pattern(const string& input_line, const string& pattern) {
-    if (pattern.length() == 1) {
-        if (pattern[0] == '.') return !input_line.empty(); // '.' should match any single char, so any non-empty string has a match
+    // Simple cases for patterns that are just one token
+    if (pattern.length() == 1 && pattern != ".") {
         return input_line.find(pattern) != string::npos;
     }
-    else if (pattern == "\\d") {
-        // Match any digit character in input_line
+    if (pattern == "\\d") {
         return input_line.find_first_of("0123456789") != string::npos;
     }
-    else if(pattern == "\\w"){
+    if (pattern == "\\w") {
         return input_line.find_first_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_") != string::npos;
     }
-    else if(pattern.size() >=4 && pattern[0] == '['  && pattern[1] == '^'&& pattern[pattern.size()-1] == ']'){
-        string str = pattern.substr(2,pattern.size()-3); // substr(position , count)
+    if (pattern.size() >= 4 && pattern[0] == '[' && pattern[1] == '^' && pattern[pattern.size() - 1] == ']') {
+        string str = pattern.substr(2, pattern.size() - 3);
         return (input_line.find_first_not_of(str) != string::npos);
     }
-    else if(pattern.size() >= 3 && pattern[0] == '[' && pattern[pattern.size()-1]== ']'){
-        string str = pattern.substr(1,pattern.size()-2);
+    if (pattern.size() >= 3 && pattern[0] == '[' && pattern[pattern.size() - 1] == ']') {
+        string str = pattern.substr(1, pattern.size() - 2);
         return input_line.find_first_of(str) != string::npos;
     }
-    else if(pattern.size() > 1 && pattern[pattern.size()-1] == '$' ){ // for eg pinapple123 is not pinapple$ because it should end with ple not 123
-        string temp = pattern.substr(0, pattern.size()-1);
 
-        if(pattern[0] == '^')
-            temp = pattern.substr(1, pattern.size()-2);
+    // This is the main matching engine for all complex patterns
+    for (int j = 0; j <= (int)input_line.length(); ++j) {
+        string sub = input_line.substr(j);
+        int ptr = 0;
+        bool flag = true;
 
-         if(input_line.size() < temp.size()) return false;
-         
-         // If both ^ and $ are present, it must be an exact match
-         if (pattern[0] == '^' && input_line.size() != temp.size()) return false;
+        for (int i = 0; i < (int)pattern.size(); i++) {
+            char ch = pattern[i];
 
-        int start = input_line.size() - temp.size(); // start comparing from end
-        for(int i = 0; i < temp.size(); i++){
-            // MODIFIED LINE 1
-            if(temp[i] != '.' && input_line[start + i] != temp[i]) return false;
-        }
-        return true;
-    }
+            if (ptr >= sub.size()) {
+                if (i + 1 < (int)pattern.size() && pattern[i + 1] == '?') {
+                    i++;
+                    continue;
+                }
+                flag = false;
+                break;
+            }
 
-    else if(pattern[0] == '^'){
-        // Check if input is long enough for the pattern (excluding '^')
-        if (input_line.length() < pattern.length() - 1) return false;
-        for(int i=1;i<pattern.size();i++){
-            // MODIFIED LINE 2
-            if(pattern[i] != '.' && pattern[i] != input_line[i-1])return false;
-        }
-        return true;
-    }
-    else if(true){
-        int len = input_line.size();
-
-        for(int j=0;j<len;j++){
-            string sub = input_line.substr(j);// substring
-            int ptr = 0;// to traverse in string 
-            bool flag = true;// false when nothing matches
-
-            for(int i=0;i<pattern.size();i++){
-                char ch =  pattern[i];
-
-                if(ptr >= sub.size()){// if exceedes
+            if (i + 1 < (int)pattern.size() && pattern[i + 1] == '+') {
+                int cnt = 0;
+                while (ptr < sub.size() && (ch == '.' || sub[ptr] == ch)) {
+                    ptr++;
+                    cnt++;
+                }
+                if (i + 2 < (int)pattern.size() && (ch == '.' || pattern[i + 2] == ch)) {
+                    if (cnt > 0) {
+                        ptr--;
+                    }
+                }
+                if (cnt == 0) {
                     flag = false;
                     break;
                 }
-                if(i+1 < pattern.size()&&pattern[i+1]  == '+'){
-                    int cnt =0;
-                    while (ptr<sub.size()  && (sub[ptr] == ch || ch == '.'))
-                    {
-                        ptr++;
-                        cnt++;
-                    }
-                    if(i+2 < pattern.size() && (pattern[i+2] == ch || ch == '.')){
-                        if(cnt > 0){
-                            ptr--;
-                        }
-                    }
-                    if(cnt == 0){
-                        flag = false;
-                        break;
-                    }
-                    i++;
-                }else if (i+1 < pattern.size() && pattern[i+1] == '?') {
-                    // It's optional, so we only advance the input pointer if it matches.
-                    if (ptr < sub.size() && (ch == '.' || sub[ptr] == ch)) {
-                        ptr++;
-                    }
-                    i++; // Always advance the pattern pointer past '?'
-                }
-                else if(ch == '\\' && i+1 < pattern.size()){
-                    char meta_char = pattern[i+1];
-                    bool matches = false;
-                    if(meta_char == 'd' && isDigit(sub[ptr])){
-                        matches = true;
-                    }else if(meta_char == 'w' && (isAlpha(sub[ptr])  || isDigit(sub[ptr]) || sub[ptr] == '_')){
-                        matches = true;
-                    }
-                    if(matches){
-                        ptr++;
-                        i++;
-                    }else{
-                        flag = false;
-                        break;
-                    }
-                }else{
-                    if(ch != '.' && ch != sub[ptr]){
-                        flag = false;
-                        break;
-                    }
+                i++;
+            } else if (i + 1 < (int)pattern.size() && pattern[i + 1] == '?') {
+                if (ptr < (int)sub.size() && (ch == '.' || sub[ptr] == ch)) {
                     ptr++;
                 }
+                i++;
+            } else if (ch == '\\' && i + 1 < pattern.size()) {
+                char meta_char = pattern[i + 1];
+                bool matches = false;
+                if (meta_char == 'd' && isdigit(sub[ptr])) {
+                    matches = true;
+                } else if (meta_char == 'w' && (isalnum(sub[ptr]) || sub[ptr] == '_')) {
+                    matches = true;
+                }
+                if (matches) {
+                    ptr++;
+                    i++;
+                } else {
+                    flag = false;
+                    break;
+                }
+            } else {
+                if (ch != '.' && ch != sub[ptr]) {
+                    flag = false;
+                    break;
+                }
+                ptr++;
             }
-            if(flag)return true;
         }
-        return false;
+        if (flag) return true;
     }
-    else {
-        throw runtime_error("Unhandled pattern " + pattern);
-    }
+    return false;
 }
 
-int main(int argc, char* argv[]) { // argc - number of argumnets && argv - array of C-style strings (the actual arguments).
-
-    // Flush after every std::cout / std::cerr
+int main(int argc, char* argv[]) {
     cout << unitbuf;
-    //disable output buffering - Normally, output waits in a buffer until flushed, but with unitbuf, everything gets printed immediately
     cerr << unitbuf;
 
-    if (argc != 3) { // if 3 argumnets return because we need 2
+    if (argc != 3) {
         cerr << "Expected two arguments" << endl;
         return 1;
     }
